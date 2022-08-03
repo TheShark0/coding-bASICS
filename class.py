156e00d920ed0b2fd401b9705c2bcb87b6568fcc
TheShark0 <109994197+TheShark0@users.noreply.github.com>
2022-08-03 16:30:05 -0500

    Add files via upload

---
+++ class.py
@@ -0,0 +1,8 @@
+sex = input("Are you a girl or boy?       ")
+
+if sex == "girl":
+    print("hey Girl, how are you ")
+if sex == "boy":
+    print("hey boy you ugly asl")
+else:
+    print("dude you shoudl sajdan")
